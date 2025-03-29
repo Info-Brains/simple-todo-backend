59fe78a36add2806aa483a9040afae5b37413c64
proXDhiya <prox.dhiya@gmail.com>
2025-03-29 23:28:11 +0100

    Session 3.1

--- app.js
+++ app.js
@@ -6,6 +6,16 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use('/health', (req, res) => {
+    console.log('Health check route hit');
+    return res.status(200).json({ status: 'UP' })
+})
+
+app.use('/error', (req, res) => {
+    console.error('Error route hit');
+    return res.status(500).json({ status: 'Error' })
+})
+
 app.use(router);
 
 app.listen(3000, () => {
